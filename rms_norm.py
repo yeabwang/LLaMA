d86924bf9869f521832085593b3731409141bbdf
yeabwang <yeabsiratesfaye781@gmail.com>
2026-08-18 12:23:11 +0800

    Add RMSNorm layer implementation

---
+++ rms_norm.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+
+class RMSNorm(nn.Module):
+    # implmenting (xi / rmsn(xi)) * gamma where rmsn(xi) = sqrt(mean(xi**2))
+    def __init__(self, dim, eps: float = 1e-6):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(dim))  # gamma
+        self.eps = eps
+
+    def forward(self, x: torch.Tensor):
+        x_float = x.float()
+
+        rms = torch.rsqrt(x_float.pow(2).mean(-1, keepdim=True) + self.eps)
+
+        output = x_float * rms
+
+        return self.weight * output.type_as(x)
